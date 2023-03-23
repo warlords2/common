7ce032599c74b48aca04796543ab860daa899f19
administrador <gilberto.tec@vivaldi.net>
2023-03-23 15:28:47 -0300

    init:bug: dist update

--- dist/game/model/province.d.ts
+++ dist/game/model/province.d.ts
@@ -1,9 +1,7 @@
-/// <reference types="node" />
 import { City } from "./city";
 import { Position } from "./geometry/position";
 export interface Province {
     name: String;
     position: Position;
-    buffs: Buffer[];
     cities: City[];
 }
